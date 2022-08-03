e248c3c8985a6488483da2785bfc3c91365f6a5f
himanshu@webtheuniverse.com <himanshu@webtheuniverse.com>
2022-08-03 18:56:22 +0530

    done 11 video of basicForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basicForm';
+  users: any = {};
+  display:boolean = true;
+  data:number = 10;
+  data2:number = 20;
+  userDetails = [
+    {name: 'Himanshu',email: 'HA@gmail.com'},     
+    {name: 'Darshit',email: 'DK@gmail.com'},
+    {name: 'Vishal',email: 'VR@gmail.com'}
+  ]
+
+  // get form value
+  getData(data: NgForm) {
+     console.log(data);
+     this.users = data;  
+  }
+   
+  toggle() {
+    this.display= this.display? false: true;
+  }
+  
+  updateData() {
+    this.data = Math.trunc(Math.random() * 100 + 1);
+  }
+
+  data3 = 'x';
+  receiveDataFromChild(item: string) {
+    console.log(item);
+    this.data3 = item;
+  }
+  // todo-list
+  /* list:any [] = [];
+  makeTodo(data: any) {
+     this.list.push({id:this.list.length, data:data.value});
+     console.log(this.list);
+     data.value = '';
+  }
+
+  deleteTodo(index: number) {
+     this.list = this.list.filter(item => item.id != index); 
+  } */
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule} from '@angular/material/button';
+import { ChildComponent } from './child/child.component';
+import { InnerChildComponent } from './child/inner-child/inner-child.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserDetails2Component } from './user-details2/user-details2.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ 
+    AppComponent, ChildComponent, InnerChildComponent, UserDetailsComponent, UserDetails2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
